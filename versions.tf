2472289455b4d3b40e3c5ff885053676ed231c4e
Mansoor Taj <tajmansoortaj@gmail.com>
2026-01-30 14:46:02 -0500

    changes

--- versions.tf
+++ versions.tf
@@ -8,7 +8,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">=3.0.1"
+      version = ">=21.0"
     }
     aws = {
       source  = "hashicorp/aws"
