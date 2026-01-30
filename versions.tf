c3489525cbc7eb6241f630cd8d62bfebc5dffdef
Mansoor Taj <tajmansoortaj@gmail.com>
2026-01-30 14:22:43 -0500

    Change randome

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.8.1"
+      version = "~> 3.1.3"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
